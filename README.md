7bb967e1751a09c1125fb49c4df04eed1f9ef619
gaerae <gaerae@gmail.com>
2018-08-07 20:06:54 +0900

    Facebook Contest Winner

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# facebook-contest-winner
-A quick and easy way to pick a winner for Facebook timeline promotion.
+![Facebook Contest Winner Title](https://1.bp.blogspot.com/-C6_kRLuy3m4/W2l4tkUWmlI/AAAAAAAANGM/0SvXoYTyksEhRG0ipEz0pgppd-YL4QP3ACLcBGAs/s1600/facebook-contest-winner-title.png)
+# Facebook Contest Winner
+A easily and quickly way to pick a winner for Facebook timeline promotion.
+
+
+## Overview
+
+📋 You can easily and quickly pick a winner by browser.
+1. Open the Facebook page or group post.
+2. Open the Facebook comment or share dialog in a new tab.
+3. Open the Console in browser.(Windows or Linux: <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>J</kbd> / macOS: <kbd>Cmd</kbd> + <kbd>Opt</kbd> + <kbd>J</kbd>)
+4. Copy and Paste all the contents of the [index.js](https://raw.githubusercontent.com/gaerae/facebook-contest-winner/master/index.js) file.
+5. Click the Button to View more comments or shares.
+6. Go to [Usage](#usage).
+
+## Usage
+
+#### 🎉 Pick a Winner from Comment
+
+```
+// Number of winners
+FacebookContestWinner.comment(3)
+
+// Designated winners is Optional Parameter
+FacebookContestWinner.comment(3, [123, 456, 789])
+```
+
+#### 🎉 Pick a Winner from Share
+
+```
+// Number of winners
+FacebookContestWinner.share(3)
+
+// Designated winners is Optional Parameter
+FacebookContestWinner.comment(3, [123, 456, 789])
+```
+
+#### 📜 Automatically scrolling to bottom of page (Optional)
+Please use it on the shared page.
+```
+FacebookContestWinner.autoScroll()
+```
+
+## Screen
+#### Result
+![Facebook Contest Winner](https://4.bp.blogspot.com/-2ueejgjX-xo/W2l2vZSYlWI/AAAAAAAANGA/DhgO9pnAeoocSia-LN3n-T23IonWYVzLgCLcBGAs/s1600/facebook-contest-winner.png)
+
+#### Facebook post
+![](https://4.bp.blogspot.com/-8lNuqe8NPVc/W2l8YJyvohI/AAAAAAAANGY/a4C2k7sordUvxfMCgf6xbYJhjlXUmdtpwCLcBGAs/s1600/facebook-contest-winner-timeline.png)
---
+++ index.js
@@ -0,0 +1,116 @@
+FacebookContestWinner = {
+  // List all commented users
+  comment(number = 5, pickUsers = []) {
+    let users = {};
+    let pickWinners = {};
+    document.querySelectorAll("div[class=permalinkPost], div[class=UFIList] div[id^=comment_]")
+      .forEach((element, index) => {
+        let user = element.querySelector(".UFICommentActorName");
+        let userId = this.parseURL(user.dataset.hovercard);
+        let tempUrl = user.href.split("?")[0];
+        users[index] = {
+          name: user.textContent,
+          id: userId,
+          url: (tempUrl.indexOf("profile.php") === -1) ? tempUrl : `${tempUrl}?id=${userId}`,
+          pic: element.querySelector(".UFIActorImage").src,
+          content: element.querySelector(".UFICommentBody").textContent,
+        };
+
+        if (pickUsers.indexOf(`${users[index].id}`) !== -1) {
+          pickWinners[`${users[index].id}`] = users[index];
+        }
+        //console.log(`%c ${users[index].name}(${users[index].id}): ${users[index].url}`, "color: #ffffff; background: #db178a; border: 1px solid #ae0c6b; border-radius: 3px;");
+      });
+    this.pick(users, number, pickWinners);
+  },
+
+  // List all shared users
+  share(number = 5, pickUsers = []) {
+    let users = {};
+    let pickWinners = {};
+    document.querySelectorAll("div[id=repost_view_permalink] div[role=article] > div[id^=u_]")
+      .forEach((element, index) => {
+        let userProfile = element.querySelector("a[class=profileLink]");
+        let userContent = element.querySelector("div > .userContent > p");
+        let userId = this.parseURL(userProfile.dataset.hovercard);
+        let tempUrl = userProfile.href.split("?")[0];
+        users[index] = {
+          name: userProfile.textContent,
+          id: userId,
+          url: (tempUrl.indexOf("profile.php") === -1) ? tempUrl : `${tempUrl}?id=${userId}`,
+          pic: element.querySelector("img[role=img]").src,
+          content: (userContent) ? userContent.textContent : "",
+        };
+
+        if (pickUsers.indexOf(`${users[index].id}`) !== -1) {
+          pickWinners[`${users[index].id}`] = users[index];
+        }
+        //console.log(`%c ${users[index].name}(${users[index].id}): ${users[index].url}`, "color: #ffffff; background: #db178a; border: 1px solid #ae0c6b; border-radius: 3px;");
+      });
+    this.pick(users, number, pickWinners);
+  },
+
+  // Pick one or multiple winners
+  pick(users, number = 5, pickWinners = {}) {
+    const userLength = Object.keys(users).length;
+    const pickWinnerLength = Object.keys(pickWinners).length;
+    let winners = {};
+    let winnerIndex = 0;
+    let winnerIDs = [];
+    console.log("%c 🎉 Winners!", "font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)");
+
+    if (pickWinnerLength > 0) {
+      for (let pickKey in pickWinners) {
+        console.log(`%c ${pickWinners[pickKey].name} ${pickWinners[pickKey].url}`, "color: #ffffff; background: #1d69db; border: 1px solid #0f4ba6; border-radius: 3px;");
+        winners[pickWinners[pickKey].id] = pickWinners[pickKey];
+        winnerIDs.push(pickWinners[pickKey].id);
+      }
+    } else {
+      do {
+        winUser = users[Math.floor(Math.random() * userLength)];
+        if (winners[winUser.id] === undefined) {
+          console.log(`%c ${winUser.name} ${winUser.url}`, "color: #ffffff; background: #1d69db; border: 1px solid #0f4ba6; border-radius: 3px;");
+          winners[winUser.id] = winUser;
+          winnerIDs.push(winUser.id);
+          winnerIndex++;
+        }
+      } while (winnerIndex < number);
+    }
+
+    console.groupCollapsed("%c ⭐ Facebook Contest Winner ⭐ ", "color: #ffffff; background: #000000; border: 1px solid #000000;");
+      console.log("Help 🙋 https://github.com/gaerae/facebook-contest-winner");
+      console.group("%c Winner Details ", "color: #ffffff; background: #db178a; border: 1px solid #ae0c6b; border-radius: 3px;");
+        console.log(winners);
+      console.groupEnd();
+      console.group("%c Optional Parameter ", "color: #ffffff; background: #db178a; border: 1px solid #ae0c6b; border-radius: 3px;");
+        console.log(winnerIDs);
+      console.groupEnd();
+    console.groupEnd();
+  },
+
+  // parse a url
+  parseURL(url = "") {
+    let tempParsing = [], tempSplit, tempUrls = url.slice(url.indexOf("?") + 1).split("&");
+    for (let i = 0; i < tempUrls.length; i++) {
+      tempSplit = tempUrls[i].split("=");
+      tempParsing[tempSplit[0]] = tempSplit[1];
+    }
+    return (tempParsing["id"]) ? tempParsing["id"] : "";
+  },
+
+  // Automatically scrolling to bottom of page
+  autoScroll() {
+    let pageY = 0;
+    let scroll = setInterval(() => {
+      window.scrollBy(0, 1000);
+      let currentPageY = window.pageYOffset;
+      if (pageY !== currentPageY) {
+        pageY = currentPageY;
+        console.log(`pageYOffset: ${pageY}`);
+      } else {
+        clearInterval(scroll);
+        console.log("At the bottom");
+      }
+    }, 2000);
+  },
+};
