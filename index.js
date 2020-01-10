d398b63ec199fa054ab3fb9475d19aaf97fb9df4
gaerae <gaerae@gmail.com>
2020-01-10 21:11:19 +0900

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,8 @@ FacebookContestWinner = {
     let winnerIndex = 0;
     let winnerIDs = [];
     if (type == 'auto') {
-      console.log("%c 🎉 Front Runner!", "font-weight: bold; font-size: 30px; color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)");
+      // clear();
+      console.log("%c 📑 Final Candidate List!", "font-weight: bold; font-size: 20px; color: #db178a;");
     } else {
       console.log("%c 🎉 Winners!", "font-weight: bold; font-size: 50px; color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)");
     }
